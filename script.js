e42686be1df1dd63fe860c9c551da4a5291192bd
Maudiel Rogers <amaudieledgardo@gmail.com>
2026-07-14 21:04:46 -0500

    Update ReMine Map design

--- script.js
+++ script.js
@@ -147,3 +147,13 @@ document.getElementById("filter-lithium").addEventListener("change", applyFilter
 document.getElementById("filter-electronics").addEventListener("change", applyFilters);
 document.getElementById("filter-verified").addEventListener("change", applyFilters);
 document.getElementById("filter-call-first").addEventListener("change", applyFilters);
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
